9998023636a92aaf402d0bc13d8c61aa1ce5000a
Robert-link <50363893+Robert-link@users.noreply.github.com>
2019-05-07 14:07:59 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+var image = 1;
+
+myImage.onclick = function(){
+	image = image === 1 ? 2 : 1 
+	if(image === 1) {
+		myImage.setAttribute('src','images/sity.png');
+	} else { 
+		myImage.setAttribute('src','images/city2.jpg');
+		}
+		
+}
+
+var city = 'Barselona';
+var myButton  = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(name) {
+	var myName = name;
+	if (!myName) {
+		var myName = prompt('Please enter your name.');
+		localStorage.setItem('name', myName);
+	}
+	myHeading.textContent = city + ' is cool, ' + myName;
+	}
+
+setUserName(localStorage.getItem('name'));
+myButton.onclick = function(){ setUserName();}
